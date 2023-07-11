a73e78e0062ebe266343ffbf2c5285884b84ae80
Martin Lukas <martinlukas24@protonmail.com>
2023-07-11 23:08:01 +0100

    Random: WIP multiplication table

---
+++ src/main/scala/org/lukas/scala/exercises/random/MultiplicationTable.scala
@@ -0,0 +1,35 @@
+package org.lukas.scala.exercises.random
+
+import org.lukas.scala.exercises.utils.FileUtils.readFileLines
+import org.scalatest.flatspec.AnyFlatSpec
+
+import java.nio.file.Files
+import scala.collection.mutable
+import scala.concurrent.Future
+import scala.util.Using
+
+class MultiplicationTable extends AnyFlatSpec {
+  type Table = Seq[Seq[Int]]
+
+  def multiTableString(): String =
+    val table = multiTable(10)
+    val maxNumber = table.map(_.max).max
+    table.map(makeRow(_, maxNumber)).mkString("\n")
+
+  def multiTable(max: Int): Table = {
+    (1 to max).map { row =>
+      (1 to max).map(_ * row)
+    }
+  }
+
+  def makeRow(row: Seq[Int], maxNumber: Int): String =
+    val maxLength = maxNumber.toString.length
+    return maxLength.toString
+
+  def padNum(num: Int, minLength: Int): String = "sdfds"
+
+  "multiTable" should "return multiplication table" in {
+    val table = multiTable(10)
+    println(table)
+  }
+}

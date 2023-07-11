b0c5bfa5d369a11bd20fea56d2c76de74d16aa57
Martin Lukas <martinlukas24@protonmail.com>
2023-07-11 23:07:32 +0100

    DailyProgrammer 399: WIP 151 len + 11 gap

--- src/main/scala/org/lukas/scala/exercises/rdailyprogrammer/Ch399LetterValueSum.scala
+++ src/main/scala/org/lukas/scala/exercises/rdailyprogrammer/Ch399LetterValueSum.scala
@@ -23,17 +23,19 @@ class Ch399LetterValueSum extends AnyFlatSpec {
     assert(letterSum("microspectrophotometries") === 317)
     println("\nOptional challenges:")
     val wordList = loadEnable1List()
-    print("Word with letter sum of 319: ")
+    print("Word with sum 319: ")
     println(wordList.find(letterSum(_) == 319).get)
-    print("No. of words with odd letter sum: ")
+    print("No. of words with odd sum: ")
     println(wordList.count(letterSum(_) % 2 == 1))
     print("Most common sum, and no. of words that have it: ")
-    val (mostCommonSum, mostCommonSumCount) = wordList
-      .map(letterSum)
-      .groupBy(identity)
-      .view
-      .mapValues(_.size)
-      .maxBy(_._2)
-    println(s"sum - $mostCommonSum, words - $mostCommonSumCount")
+    val (mostCommonSum, mostCommonSumWords) = wordList.groupBy(letterSum).maxBy(_._2.size)
+    println(s"sum - $mostCommonSum, words - ${mostCommonSumWords.size}")
+    print("Words with sum 151 but their lengths differ by 11 letters: ")
+// zyzzyva and biodegradabilities
+// answer:
+//    wordList.groupBy(_.length)
+    // 15 -> List(sdjlkf, sdjklf,sdlfk)
+//    (minLen to (maxLen - 11))
+
   }
 }
